996b5f3ed7558c2556fb61d1c853af61cc4d3cfd
messense <messense@icloud.com>
2022-02-16 14:34:04 +0800

    Pass arguments to cargo build

--- src/cli.rs
+++ src/cli.rs
@@ -1,8 +1,11 @@
 use std::path::PathBuf;
+use std::process::{self, Command};
+use std::str;
 
+use anyhow::{bail, format_err, Context, Result};
 use clap::Parser;
 
-use crate::zig::Zig;
+use crate::zig::{prepare_zig_linker, Zig};
 
 /// Compile a local package and all of its dependencies
 /// using zig as linker
@@ -95,7 +98,7 @@ pub struct Build {
 
     /// Build for the target triple
     #[clap(long, value_name = "TRIPLE")]
-    pub target: Option<String>,
+    target: Option<String>,
 
     /// Directory for all generated artifacts
     #[clap(long, value_name = "DIRECTORY", parse(from_os_str))]
@@ -131,7 +134,7 @@ pub struct Build {
 
     /// Use verbose output (-vv very verbose/build.rs output)
     #[clap(short = 'v', long, parse(from_occurrences), max_occurrences = 2)]
-    verbose: u64,
+    verbose: usize,
 
     /// Coloring: auto, always, never
     #[clap(long, value_name = "WHEN")]
@@ -169,4 +172,176 @@ pub enum Opt {
     Build(Build),
     #[clap(subcommand)]
     Zig(Zig),
+}
+
+impl Build {
+    pub fn execute(&self) -> Result<()> {
+        let mut build = Command::new("cargo");
+        build.arg("build");
+
+        let target = if let Some(target) = self.target.as_ref() {
+            build.arg("--target").arg(target);
+            target.clone()
+        } else {
+            get_host_target()?
+        };
+
+        // collect cargo build argments
+        if self.quiet {
+            build.arg("--quiet");
+        }
+        for pkg in &self.packages {
+            build.arg("--package").arg(pkg);
+        }
+        if self.workspace {
+            build.arg("--workspace");
+        }
+        for item in &self.exclude {
+            build.arg("--excude").arg(item);
+        }
+        if self.all {
+            build.arg("--all");
+        }
+        if let Some(jobs) = self.jobs {
+            build.arg("--jobs").arg(jobs.to_string());
+        }
+        if self.lib {
+            build.arg("--lib");
+        }
+        for bin in &self.bin {
+            build.arg("--bin").arg(bin);
+        }
+        if self.bins {
+            build.arg("--bins");
+        }
+        for example in &self.example {
+            build.arg("--example").arg(example);
+        }
+        if self.examples {
+            build.arg("--examples");
+        }
+        for test in &self.test {
+            build.arg("--test").arg(test);
+        }
+        if self.tests {
+            build.arg("--tests");
+        }
+        for bench in &self.bench {
+            build.arg("--bench").arg(bench);
+        }
+        if self.benches {
+            build.arg("--benches");
+        }
+        if self.all_targets {
+            build.arg("--all-targets");
+        }
+        if self.release {
+            build.arg("--release");
+        }
+        if let Some(profile) = self.profile.as_ref() {
+            build.arg("--profile").arg(profile);
+        }
+        for feature in &self.features {
+            build.arg("--features").arg(feature);
+        }
+        if self.all_features {
+            build.arg("--all-features");
+        }
+        if self.no_default_features {
+            build.arg("--no-default-features");
+        }
+        if let Some(dir) = self.target_dir.as_ref() {
+            build.arg("--target-dir").arg(dir);
+        }
+        if let Some(dir) = self.out_dir.as_ref() {
+            build.arg("--out-dir").arg(dir);
+        }
+        if let Some(path) = self.manifest_path.as_ref() {
+            build.arg("--manifest-path").arg(path);
+        }
+        if self.ignore_rust_version {
+            build.arg("--ignore-rust-version");
+        }
+        for fmt in &self.message_format {
+            build.arg("--message-format").arg(fmt);
+        }
+        if self.build_plan {
+            build.arg("--build-plan");
+        }
+        if self.unit_graph {
+            build.arg("--unit-graph");
+        }
+        if self.future_incompat_report {
+            build.arg("--future-incompat-report");
+        }
+        if self.verbose > 0 {
+            build.arg(format!("-{}", "v".repeat(self.verbose)));
+        }
+        if let Some(color) = self.color.as_ref() {
+            build.arg("--color").arg(color);
+        }
+        if self.frozen {
+            build.arg("--frozen");
+        }
+        if self.locked {
+            build.arg("--locked");
+        }
+        if self.offline {
+            build.arg("--offline");
+        }
+        for config in &self.config {
+            build.arg("--config").arg(config);
+        }
+        for flag in &self.unstable_flags {
+            build.arg("-Z").arg(flag);
+        }
+
+        // setup zig as linker
+        let (zig_cc, zig_cxx) = prepare_zig_linker(&target)?;
+        let env_target = target.to_uppercase().replace("-", "_");
+        build.env("TARGET_CC", &zig_cc);
+        build.env("TARGET_CXX", &zig_cxx);
+        build.env(format!("CARGO_TARGET_{}_LINKER", env_target), &zig_cc);
+
+        let mut child = build.spawn().context("Failed to run cargo build")?;
+        let status = child.wait().expect("Failed to wait on cargo build process");
+        if !status.success() {
+            process::exit(status.code().unwrap_or(1));
+        }
+        Ok(())
+    }
+}
+
+fn get_host_target() -> Result<String> {
+    let output = Command::new("rustc").arg("-vV").output();
+    let output = match output {
+        Err(err) if err.kind() == std::io::ErrorKind::NotFound => {
+            bail!(
+                "rustc, the rust compiler, is not installed or not in PATH. \
+                This package requires Rust and Cargo to compile extensions. \
+                Install it through the system's package manager or via https://rustup.rs/.",
+            );
+        }
+        Err(err) => {
+            return Err(err).context("Failed to run rustc to get the host target");
+        }
+        Ok(output) => output,
+    };
+
+    let output = str::from_utf8(&output.stdout).context("`rustc -vV` didn't return utf8 output")?;
+
+    let field = "host: ";
+    let host = output
+        .lines()
+        .find(|l| l.starts_with(field))
+        .map(|l| &l[field.len()..])
+        .ok_or_else(|| {
+            format_err!(
+                "`rustc -vV` didn't have a line for `{}`, got:\n{}",
+                field.trim(),
+                output
+            )
+        })?
+        .to_string();
+    Ok(host)
 }
