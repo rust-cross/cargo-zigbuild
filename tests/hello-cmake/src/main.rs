f93423986f0c9bfb6ad4ec1b28702ec7461fdcd3
messense <messense@icloud.com>
2022-09-14 22:07:15 +0800

    Add cmake test case

---
+++ tests/hello-cmake/src/main.rs
@@ -0,0 +1,10 @@
+use std::ffi::CStr;
+
+use libz_sys::zlibVersion;
+
+fn main() {
+    let ver = unsafe { zlibVersion() };
+    let ver_cstr = unsafe { CStr::from_ptr(ver) };
+    let version = ver_cstr.to_str().unwrap();
+    assert!(!version.is_empty());
+}
