36025168b675b5c0efe83348f713c7892af9fb46
messense <messense@icloud.com>
2022-11-07 20:28:45 +0800

    Add support for `gnullvm` target environment

--- src/zig.rs
+++ src/zig.rs
@@ -611,6 +611,7 @@ pub fn prepare_zig_linker(target: &str) -> Result<ZigWrapper> {
     let arch = triple.architecture.to_string();
     let target_env = match (triple.architecture, triple.environment) {
         (Architecture::Mips32(..), Environment::Gnu) => Environment::Gnueabihf,
+        (_, Environment::GnuLlvm) => Environment::Gnu,
         (_, environment) => environment,
     };
     let file_ext = if cfg!(windows) { "bat" } else { "sh" };
