ecb93c3d1cdb23249febf92e58e7eff164a7dc05
messense <messense@icloud.com>
2023-03-30 09:54:33 +0800

    Filter out unsupported `-Wl,--dynamicbase` link arg for Windows gnu targets
    xref #96

--- src/zig.rs
+++ src/zig.rs
@@ -102,11 +102,13 @@ impl Zig {
                 } else if arg == "-lwindows" || arg == "-l:libpthread.a" || arg == "-lgcc" {
                     return None;
                 } else if arg == "-Wl,--disable-auto-image-base"
+                    || arg == "-Wl,--dynamicbase"
                     || arg == "-Wl,--large-address-aware"
                 {
                     // https://github.com/rust-lang/rust/blob/f0bc76ac41a0a832c9ee621e31aaf1f515d3d6a5/compiler/rustc_target/src/spec/windows_gnu_base.rs#L23
+                    // https://github.com/rust-lang/rust/blob/2fb0e8d162a021f8a795fb603f5d8c0017855160/compiler/rustc_target/src/spec/windows_gnu_base.rs#L22
                     // https://github.com/rust-lang/rust/blob/f0bc76ac41a0a832c9ee621e31aaf1f515d3d6a5/compiler/rustc_target/src/spec/i686_pc_windows_gnu.rs#L16
-                    // zig doesn't support --disable-auto-image-base, --large-address-aware
+                    // zig doesn't support --disable-auto-image-base, --dynamicbase, and --large-address-aware
                     return None;
                 }
             } else if arg == "-Wl,--no-undefined-version" {
