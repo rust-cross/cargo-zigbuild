25a112357ca98a8a7d8dcb147225426d8801b3bd
messense <messense@icloud.com>
2023-11-21 22:50:50 +0800

    Update cargo-options to 0.7.1

--- src/bin/cargo-zigbuild.rs
+++ src/bin/cargo-zigbuild.rs
@@ -8,7 +8,12 @@ use clap::Parser;
 
 #[allow(clippy::large_enum_variant)]
 #[derive(Debug, Parser)]
-#[command(version, name = "cargo-zigbuild", display_order = 1)]
+#[command(
+    version,
+    name = "cargo-zigbuild",
+    display_order = 1,
+    styles = cargo_options::styles(),
+)]
 pub enum Opt {
     #[command(name = "zigbuild", aliases = &["build", "b"] )]
     Build(Build),
